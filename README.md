6962dc9c464bd19761f066459b5b1278980764ed
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2021-02-27 22:11:27 +0100

    Menu example

---
+++ README.md
@@ -0,0 +1,18 @@
+# SSD1306 Display Bring-up
+By Giulio Corradini
+
+## Libraries
+Using PlatformIO and ESP-IDF we can use lexus2k/ssd1306 library to talk to the display.
+
+Since it's a ESP-IDF component we must create a folder in project's root directory named
+`components` and clone/submodule lib's repository there.
+
+To build and link against it you should set `COMPONENT_DIR` variable in your root's CMakeLists.txt
+using this directive: `set(COMPONENT_DIR components)`.
+
+## Use hardware I2C in custom configuration
+lexus2k/ssd1306 uses a default I2C controller with its default pins, to configure your own
+you must call `ssd1306_platform_i2cInit` defined in header `ssd1306_hal/io.h`. The right functions
+for your platform are automagically loaded by the preprocessor.
+
+Examples are located in /examples directory.
--- src/main.c
+++ src/main.c
@@ -1,1 +1,69 @@
-void app_main() {}
+#include <stdio.h>
+
+#include "driver/gpio.h"
+
+#include "driver/i2c.h"
+#include "ssd1306_hal/io.h"
+#include "ssd1306.h"
+
+struct locked_menu {
+    SAppMenu *menu;
+    SemaphoreHandle_t lock;
+};
+
+void button_responder_task(void *pvParameters) {
+
+    struct locked_menu *lm = (struct locked_menu *)pvParameters;
+    
+    gpio_pad_select_gpio(0);
+    gpio_set_direction(0, GPIO_MODE_INPUT);
+
+    while(true) {
+
+        if(gpio_get_level(0) == 0) {
+            if(xSemaphoreTake(lm->lock, 10)) {
+                ssd1306_menuDown(lm->menu);
+                xSemaphoreGive(lm->lock);
+            }
+        }
+
+        vTaskDelay(100 / portTICK_RATE_MS);
+    }
+
+    vTaskDelete(NULL);
+}
+
+void app_main() {
+    ssd1306_setFixedFont(ssd1306xled_font6x8);
+
+    ssd1306_platform_i2cConfig_t cfg = {
+        .sda = 21,
+        .scl = 22
+    };
+    ssd1306_platform_i2cInit(I2C_NUM_0, 0, &cfg);
+
+    ssd1306_128x64_i2c_init();
+
+    ssd1306_clearScreen();
+
+    SAppMenu menu;
+    const char *menuItems[] = {
+        "WiFi",
+        "Bluetooth",
+        "Airplane mode",
+        "Other..."
+    };
+    ssd1306_createMenu(&menu, menuItems, sizeof(menuItems) /sizeof(char *));
+    ssd1306_showMenu(&menu);
+
+    SemaphoreHandle_t menuSem = xSemaphoreCreateBinary();
+    struct locked_menu task_params = {.menu = &menu, .lock = menuSem};
+    xTaskCreate(button_responder_task, "button", 1024, &task_params, 1, NULL);
+
+    while(true) {
+        xSemaphoreTake(menuSem, 10);
+        ssd1306_showMenu(&menu);
+        xSemaphoreGive(menuSem);
+        vTaskDelay(100 / portTICK_RATE_MS);
+    }
+}
