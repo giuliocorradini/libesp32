4225cc6567ecc651f3f086a10c7bc46df5b01578
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2021-04-12 15:50:04 +0200

    Getting compensation params

--- lib/bmp280/bmp280.c
+++ lib/bmp280/bmp280.c
@@ -1,14 +1,14 @@
 #include "bmp280.h"
 #include "driver/i2c.h"
 
-esp_err_t bmp280_read_register(uint8_t addr, uint8_t reg, uint8_t *dst, int n) {
+esp_err_t bmp280_read_register(uint8_t reg, uint8_t *dst, int n) {
 
     esp_err_t err;
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
     
     //  Write pointer to register (it'll be read in the next transaction)
     i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, (addr << 1 | I2C_MASTER_WRITE), true);
+    i2c_master_write_byte(cmd, (BMP280_ADDR << 1 | I2C_MASTER_WRITE), true);
     i2c_master_write_byte(cmd, reg, true);
     i2c_master_stop(cmd);
 
@@ -19,7 +19,7 @@ esp_err_t bmp280_read_register(uint8_t addr, uint8_t reg, uint8_t *dst, int n) {
     cmd = i2c_cmd_link_create();
 
     i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, (addr << 1 | I2C_MASTER_READ), true);
+    i2c_master_write_byte(cmd, (BMP280_ADDR << 1 | I2C_MASTER_READ), true);
     i2c_master_read(cmd, dst, n, true);
     i2c_master_stop(cmd);
 
@@ -30,7 +30,20 @@ esp_err_t bmp280_read_register(uint8_t addr, uint8_t reg, uint8_t *dst, int n) {
 
 int bmp280_get_id() {
     int id = 0;
-    ESP_ERROR_CHECK(bmp280_read_register(BMP280_ADDR, 0xD0, &id, 1));
+    ESP_ERROR_CHECK(bmp280_read_register(0xD0, &id, 1));
 
     return id;
+}
+
+esp_err_t bmp280_get_compensation_params(struct bmp280_compensation_params *p) {
+    esp_err_t err = ESP_OK;
+
+    uint8_t base_addr = 0x88;
+
+    for(uint8_t i = 0; i < 24; i++) {
+        err |= bmp280_read_register(base_addr + i, &p + i, 1);
+        vTaskDelay(100 / portTICK_RATE_MS);
+    }
+
+    return err;
 }
--- lib/bmp280/bmp280.h
+++ lib/bmp280/bmp280.h
@@ -5,7 +5,23 @@
 
 #define BMP280_ADDR 0x76
 
-esp_err_t bmp280_read_register(uint8_t addr, uint8_t reg, uint8_t *dst, int n);
+struct __attribute__((__packed__)) bmp280_compensation_params {
+    unsigned short dig_T1;
+    signed short dig_T2;
+    signed short dig_T3;
+    unsigned short dig_P1;
+    signed short dig_P2;
+    signed short dig_P3;
+    signed short dig_P4;
+    signed short dig_P5;
+    signed short dig_P6;
+    signed short dig_P7;
+    signed short dig_P8;
+    signed short dig_P9;
+};
+
+esp_err_t bmp280_read_register(uint8_t reg, uint8_t *dst, int n);
 int bmp280_get_id();
+esp_err_t bmp280_get_compensation_params(struct bmp280_compensation_params *p);
 
 #endif
